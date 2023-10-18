4785449bddd481caab03fca287fc042702b682d4
thisfetch1591 <thisfetch1591@gmail.com>
2023-10-18 23:46:15 +0900

    dp 2문제

---
+++ free/11057.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long dp[1002][1002];
+int main(void)
+{
+  ios::sync_with_stdio(0), cin.tie(0);
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < 10; i++)
+    dp[1][i] = 1;
+
+  for (int i = 2; i <= n; j++)
+  {
+  }
+}
