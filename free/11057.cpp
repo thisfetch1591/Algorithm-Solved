99ace0be38795f53fe2bb68c3fcfabe65bcef6a4
thisfetch1591 <thisfetch1591@gmail.com>
2023-10-19 23:45:16 +0900

    이분탐색 1문제

--- free/11057.cpp
+++ free/11057.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 long long dp[1002][1002];
+long long ans;
 int main(void)
 {
   ios::sync_with_stdio(0), cin.tie(0);
@@ -11,7 +12,20 @@ int main(void)
   for (int i = 0; i < 10; i++)
     dp[1][i] = 1;
 
-  for (int i = 2; i <= n; j++)
+  for (int i = 2; i <= n; i++)
   {
+    for (int j = 0; j < 10; j++)
+    {
+      for (int k = 0; k <= j; k++)
+      {
+        dp[i][j] += dp[i - 1][k];
+        dp[i][j] = dp[i][j] % 10007;
+      }
+    }
   }
+
+  for (int i = 0; i < 10; i++)
+    ans += dp[n][i];
+
+  cout << ans % 10007;
 }
---
+++ free/1654.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int k, n;
+int arr[10002];
+
+bool solve(long long mid)
+{
+  int tmp = 0;
+  for (int i = 0; i < n; i++)
+  {
+    
+  }
+}
+int main(void)
+{
+  ios::sync_with_stdio(0), cin.tie(0);
+  cin >> k >> n;
+
+  for (int i = 0; i < k; i++)
+    cin >> arr[i];
+
+  long long st = 0;
+  long long en = __LONG_LONG_MAX__;
+
+  while (st < en)
+  {
+    long long mid = (st + en) / 2;
+  }
+}
