9ab59f2769e34a3c72aa1908cd5fbc54c16083c5
thisfetch1591 <thisfetch1591@gmail.com>
2023-10-23 17:44:24 +0900

    이분탐색 1문제

--- free/1654.cpp
+++ free/1654.cpp
@@ -1,29 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+int arr[1000002];
 int k, n;
-int arr[10002];
-
-bool solve(long long mid)
-{
-  int tmp = 0;
-  for (int i = 0; i < n; i++)
-  {
-    
-  }
-}
 int main(void)
 {
   ios::sync_with_stdio(0), cin.tie(0);
+
   cin >> k >> n;
 
   for (int i = 0; i < k; i++)
     cin >> arr[i];
 
-  long long st = 0;
-  long long en = __LONG_LONG_MAX__;
+  long long st = 1;
+  long long ed = 0x7fffffff;
 
-  while (st < en)
+  while (st < ed)
   {
-    long long mid = (st + en) / 2;
+    int tmp = 0;
+    long long mid = (st + ed + 1) / 2;
+
+    for (int i = 0; i < k; i++)
+      tmp += arr[i] / mid;
+
+    if (tmp >= n)
+      st = mid;
+    if (tmp < n)
+      ed = mid - 1;
   }
+
+  cout << st;
 }
