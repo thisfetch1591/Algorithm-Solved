7d5ebf856a34c9a8d1fdd3ceef163046f7fd7224
thisfetch1591 <thisfetch1591@gmail.com>
2023-10-28 20:18:48 +0900

    binary search

---
+++ free/tmp.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[10002];
+vector<int> two;
+int main(void)
+{
+  ios::sync_with_stdio(0), cin.tie(0);
+
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
+
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      if (j == i)
+      {
+        
+      }
+    }
+  }
+}
