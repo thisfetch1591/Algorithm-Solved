bb2b355d30fcda1620cac2f478f9d9d4446f81f2
thisfetch1591 <thisfetch1591@gmail.com>
2023-11-10 19:27:09 +0900

    solved : 그리디 행복유치원

--- baekjoon/greedy/13164.cpp
+++ baekjoon/greedy/13164.cpp
@@ -2,46 +2,22 @@
 using namespace std;
 int n, k;
 long long arr[300002];
-long long ans;
+vector<long long> v;
+int ans;
 int main(void) {
   ios::sync_with_stdio(0), cin.tie(0);
   cin >> n >> k;
-
-  int cnt = n / k;
-
-  if (n % k != 0) {
-    cnt++;
-  }
-
   for (int i = 0; i < n; i++) {
     cin >> arr[i];
   }
-
+  sort(arr, arr + n);
   for (int i = 0; i < n; i++) {
-    long long minNum = __LONG_LONG_MAX__;
-    long long maxNum = 0;
-    for (int j = i; j < i + cnt; j++) {
-      if (maxNum < arr[j]) {
-        maxNum = arr[j];
-      }
-      if (minNum > arr[j]) {
-        minNum = arr[j];
-      }
-
-      if (j == n - 1) {
-        ans += maxNum - minNum;
-        i = n - 1;
-        break;
-      }
-
-      if (j == i + cnt - 1) {
-        ans += maxNum - minNum;
-        i = j + 1;
-        minNum = __LONG_LONG_MAX__;
-        maxNum = 0;
-      }
-    }
+    if (i == n - 1) break;
+    v.push_back(arr[i + 1] - arr[i]);
   }
-
+  sort(v.begin(), v.end());
+  for (int i = 0; i < k - 1; i++) {
+    ans += v[i];
+  }
   cout << ans;
 }
