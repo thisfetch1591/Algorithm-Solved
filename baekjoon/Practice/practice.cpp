21bcc46e9b9ffc363aa2a58dc5f8fdfadf354d02
thisfetch1591 <thisfetch1591@gmail.com>
2023-12-02 23:47:36 +0900

    practice and split

---
+++ baekjoon/Practice/practice.cpp
@@ -0,0 +1,86 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int a[10], n;
+// int main() {
+//   for (int i : a) {
+//     cout << i << " ";
+//   }
+//   return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int n;
+// int main() {
+//   cin >> n;
+//   int a[n];
+//   memset(a, 0, sizeof(a));
+//   for (int i : a) {
+//     cout << i << " ";
+//   }
+//   return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() {
+//   string s = "1";
+//   string s2 = "amumu";
+//   cout << atoi(s.c_str()) << '\n';
+//   cout << atoi(s2.c_str()) << '\n';
+//   return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() {
+//   int a = -1;
+//   cout << bool(a) << '\n';
+//   a = 0;
+//   cout << bool(a) << '\n';
+//   a = 3;
+//   cout << bool(a) << '\n';
+
+//   return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() {
+//   int a = 3;
+//   int b = 2;
+//   cout << a / b << '\n';
+//   double c = 3;
+//   double d = 2;
+//   cout << c / d << '\n';
+
+//   return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() {
+//   int a = 2147483647;
+//   cout << a << '\n';
+//   a++;
+//   cout << a << '\n';
+//   return 0;
+//}
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() {
+//   int a = -2147483648;
+//   cout << a << '\n';
+//   a--;
+//   cout << a << '\n';
+//   return 0;
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  vector<int> a = {1, 2, 3};
+  cout << (int)a.size() - 10 << '\n';
+  return 0;
+}
