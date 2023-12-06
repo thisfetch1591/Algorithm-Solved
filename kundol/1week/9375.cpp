bdaa9e613d1e12bb2af92251950c73fa1160ce48
thisfetch1591 <thisfetch1591@gmail.com>
2023-12-06 19:03:07 +0900

    1week solved

--- kundol/1week/9375.cpp
+++ kundol/1week/9375.cpp
@@ -1,17 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int tc, n;
+string a, b;
 int main(void) {
   ios::sync_with_stdio(0), cin.tie(0);
   cin >> tc;
-
   while (tc--) {
-    map<string, string> mp;
+    map<string, int> _map;
+    cin >> n;
     for (int i = 0; i < n; i++) {
-      string k, v;
-      cin >> k, v;
-
-      mp.insert(k, v);
+      cin >> a >> b;
+      _map[b]++;
     }
+    long long ret = 1;
+    for (auto c : _map) {
+      ret *= ((long long)c.second + 1);
+    }
+    cout << ret - 1 << '\n';
   }
 }
