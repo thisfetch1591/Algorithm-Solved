cf3ac63993aef02375ceb0e57d370335be8cf849
thisfetch1591 <thisfetch1591@gmail.com>
2023-12-13 23:43:53 +0900

    2weeks solved

---
+++ kundol/2week/1068.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+vector<int> adj[54];
+int main(void) {
+  ios::sync_with_stdio(0), cin.tie(0);
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    
+  }
+}
--- kundol/2week/14502.cpp
+++ kundol/2week/14502.cpp
@@ -7,6 +7,34 @@ vector<pair<int, int>> virustList, wallList;
 const int dx[] = {-1, 1, 0, 0};
 const int dy[] = {0, 0, -1, 1};
 
+void dfs(int x, int y) {
+  for (int dir = 0; dir < 4; dir++) {
+    int nx = x + dx[dir];
+    int ny = y + dy[dir];
+    if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+    if (vis[nx][ny] || arr[nx][ny]) continue;
+    vis[nx][ny] = 1;
+    dfs(nx, ny);
+  }
+  return;
+}
+
+int solve() {
+  fill(&vis[0][0], &vis[0][0] + 10 * 10, 0);
+  for (auto b : virustList) {
+    vis[b.first][b.second] = 1;
+    dfs(b.first, b.second);
+  }
+
+  int cnt = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (arr[i][j] == 0 && !vis[i][j]) cnt++;
+    }
+  }
+  return cnt;
+}
+
 int main(void) {
   ios::sync_with_stdio(0), cin.tie(0);
   cin >> n >> m;
@@ -22,7 +50,15 @@ int main(void) {
   for (int i = 0; i < wallList.size(); i++) {
     for (int j = 0; j < i; j++) {
       for (int k = 0; k < j; k++) {
-            }
+        arr[wallList[i].first][wallList[i].second] = 1;
+        arr[wallList[j].first][wallList[j].second] = 1;
+        arr[wallList[k].first][wallList[k].second] = 1;
+        ret = max(ret, solve());
+        arr[wallList[i].first][wallList[i].second] = 0;
+        arr[wallList[j].first][wallList[j].second] = 0;
+        arr[wallList[k].first][wallList[k].second] = 0;
+      }
     }
   }
+  cout << ret << '\n';
 }
