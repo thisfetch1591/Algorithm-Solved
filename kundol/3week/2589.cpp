726783e0412ee51c7f68cfd8d318f68720472096
thisfetch1591 <thisfetch1591@gmail.com>
2023-12-21 23:58:05 +0900

    3weeks solved

---
+++ kundol/3week/2589.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+string board[52];
+int main(void) {
+  ios::sync_with_stdio(0), cin.tie(0);
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) {
+    cin >> board[i];
+  }
+}
