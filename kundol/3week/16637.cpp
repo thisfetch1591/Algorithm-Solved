a83a2e3e6cc1a0921446d53e6ead10f869e47d80
thisfetch1591 <thisfetch1591@gmail.com>
2024-01-05 22:30:06 +0900

    solved 3weeks

--- kundol/3week/16637.cpp
+++ kundol/3week/16637.cpp
@@ -1,36 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX = 200000;
-int visited[MAX + 4];
-long long cnt[MAX + 4];
+int n, ret = -98765432;
+string s;
+vector<int> num;
+vector<char> oper_str;
+int oper(char a, int b, int c) {
+  if (a == '+') return b + c;
+  if (a == '-') return b - c;
+  if (a == '*') return b * c;
+}
+
+void go(int here, int _num) {
+  if (here == num.size() - 1) {
+    ret = max(ret, _num);
+    return;
+  }
+
+  go(here + 1, oper(oper_str[here], _num, num[here + 1]));
+  if (here + 2 <= num.size() - 1) {
+    int temp = oper(oper_str[here + 1], num[here + 1], num[here + 2]);
+    go(here + 2, oper(oper_str[here], _num, temp));
+  }
+  return;
+}
+
 int main() {
-  int n, m;
-  cin >> n >> m;
-  if (n == m) {
-    puts("0");
-    puts("1");
-    return 0;
+  ios::sync_with_stdio(0), cin.tie(0);
+  cin >> n;
+  cin >> s;
+  for (int i = 0; i < n; i++) {
+    if (i % 2 == 0)
+      num.push_back(s[i] - '0');
+    else
+      oper_str.push_back(s[i]);
   }
-  visited[n] = 1;
-  cnt[n] = 1;
-  queue<int> q;
-  q.push(n);
-  while (!q.empty()) {
-    int now = q.front();
-    q.pop();
-    for (int next : {now - 1, now + 1, now * 2}) {
-      if (0 <= next && next <= MAX) {
-        if (!visited[next]) {
-          q.push(next);
-          visited[next] = visited[now] + 1;
-          cnt[next] += cnt[now];
-        } else if (visited[next] == visited[now] + 1) {
-          cnt[next] += cnt[now];
-        }
-      }
-    }
-  }
-  cout << visited[m] - 1 << '\n';
-  cout << cnt[m] << '\n';
-  return 0;
 }
