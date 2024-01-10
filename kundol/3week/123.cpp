3ba763fcc20b26c0879679ab5fa9b0ae6dea1c9e
thisfetch1591 <thisfetch1591@gmail.com>
2024-01-10 17:11:10 +0900

    3weeks

---
+++ kundol/3week/123.cpp
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, a[54][54], result = 987654321;
+vector<vector<int>> chickenList;
+vector<pair<int, int>> _home, chicken;
---
+++ kundol/3week/14497.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, x1, y1, x2, y2;
+string board[302];
+int main(void) {
+  ios::sync_with_stdio(0), cin.tie(0);
+  cin >> n >> m >> x1 >> y1 >> x2 >> y2;
+
+  for (int i = 0; i < n; i++) {
+    cin >> board[i];
+  }
+}
